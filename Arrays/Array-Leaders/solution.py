9e6b6a80d50649b33cfd0e406ef79a518c8cd044
Aryan <aryanatwork45@gmail.com>
2026-08-30 01:07:20 +0530

    Add Array Leaders solution

---
+++ Arrays/Array-Leaders/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def leaders(self, arr):
+        # code here
+        ans = []
+        highest = float('-inf')
+        for i in range(len(arr) - 1, -1, -1):
+            if (arr[i] >= highest):
+                ans.append(arr[i])
+                highest = arr[i]
+        return ans[::-1]
